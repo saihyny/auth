785d9eced90dea9d3ea909e4d6ad8155c8c9d522
saihyny <satyasaikiranrocks@gmail.com>
2024-02-24 15:55:01 +0530

    Login

---
+++ src/ExpenseTracker/Expense.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Expense() {
+  return (
+    <div>welcome to Expense Tracker</div>
+  )
+}
+
+export default Expense
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,22 +1,57 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState,useEffect } from "react";
 import "./LoginPage.css";
-function LoginPage() {
+import Expense from "./ExpenseTracker/Expense";
+import {useNavigate} from 'react-router-dom'
+const LoginPage=()=> {
+ 
+  const Navigate = useNavigate()
   const [isLogin, setLoggedin] = useState(false);
   const [isnew,setUser] = useState(false);
+  const [error, setError] = useState(null);
+
   const email_log = useRef();
   const password_log = useRef();
+
 
   const email_sig = useRef();
   const password_sig = useRef();
   const confirm_sig = useRef();
+ 
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError(false);
+      }, 3000);
+
+      // Cleanup function to clear the timer when component unmounts or state changes
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const Email = isLogin ? email_log.current.value : email_sig.current.value;
-    const Password = isLogin ? password_log.current.value : password_sig.current.value;
-
+    
+    if(!isnew){
+      if(password_sig.current.value!==confirm_sig.current.value){
+        setError('please enter same password in both fields')
+        return null
+      }
+    }
+    // let Email;
+    // let Password;
+    // // if(!isnew){
+    // //   Email=email_sig.current.value;
+    // //   Password=password_sig.current.value;
+    // // }
+    // //   Email=email_log.current.value;
+    // //   Password=password_log.current.value;
+    
+    // console.log(Email,Password)
+    const Email = isnew ? email_log.current.value : email_sig.current.value;
+    const Password = isnew ? password_log.current.value : password_sig.current.value;
+    console.log(Email,Password,isnew)
     let URL;
-    if (isLogin) {
+    if (isnew) {
       URL =
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAN1w9QjbLVYdwm9PeodDq_kWATHQr6-0Y";
     } else {
@@ -37,14 +72,18 @@ function LoginPage() {
     })
       .then((res) => {
         if (res.ok) {
-          alert('successfully loggedin ')
+          !isnew ? alert('successfully sigup ') :
+          // localStorage.setItem(log,'true')
+          Navigate('/expense')
           console.log('logged in successfully')
           return res.json();
          
         } else {
           return res.json().then((data) => {
-            let errorMessage = "authentication faild";
-            throw new Error(errorMessage);
+            let errorMessage = data;
+            setError(errorMessage.error.message)
+            throw new Error(errorMessage.error.message);
+            
           });
         }
       })
@@ -52,11 +91,13 @@ function LoginPage() {
         setLoggedin(true);
       })
       .catch((err) => {
-        alert(err.message);
+        console.log(err)
+       
       });
   };
   const login = (
     <>
+      <h6>{error}</h6>
       <label>Email</label>
       <input type="email" ref={email_log} required/>
       <label>Password</label>
@@ -70,7 +111,7 @@ function LoginPage() {
   );
   const signUp = (
     <>
-     
+     <h6>{error}</h6>
       <label>Email</label>
       <input type="email" ref={email_sig}     required/>
       <label>Password</label>
