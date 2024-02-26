e79643e13ecfd9a7350d82c67fab2ddb24aeee2c
saihyny <satyasaikiranrocks@gmail.com>
2024-02-26 11:25:08 +0530

    loggout

--- src/ExpenseTracker/Expense.js
+++ src/ExpenseTracker/Expense.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Expense.css'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 function Expense() {
     const id = localStorage.getItem('login')
+    const Navigate = useNavigate()
   return (
     <div className='nav'>
         welcome to Expense Tracker
       <div className='profile'>
         your profile is incomplere<Link to='/user'>Complete Now</Link>
-       
+  
       </div>
+      <button onClick={()=>{
+        localStorage.clear('login')
+        Navigate('/')}}>Loggout</button>
     </div>
   )
 }
